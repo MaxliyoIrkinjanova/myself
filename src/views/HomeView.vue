7b54330f4429cebb262f6640ae62bdc6090b17ed
Maxliyo25 <maxliyoirkinjanova@gmail.com>
2024-06-19 17:50:06 +0500

    uzvekistan so'zini yozdim

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,7 @@
-<script setup>
-</script>
+<script setup></script>
 
 <template>
   <main>
-    <h1 class="text-3xl text-red-400">Hello</h1>
+    <h1 class="text-3xl text-red-400">Hello uzbekistan</h1>
   </main>
 </template>
